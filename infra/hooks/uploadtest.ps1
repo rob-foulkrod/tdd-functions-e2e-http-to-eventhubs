d4941285326345ff3eaf49f32d6601f70a8d67f9
Rob Foulkrod <me@robteaches.com>
2025-10-16 16:09:58 -0400

    Refactor environment variable retrieval in uploadtest.ps1 to use azd commands

--- infra/hooks/uploadtest.ps1
+++ infra/hooks/uploadtest.ps1
@@ -1,11 +1,10 @@
 # Read variables from environment variables
-azd env get-values > .env
 
-$resourceGroupName = $env:RESOURCE_GROUP
-$loadTestResourceName = $env:LOADTESTING_NAME
+$resourceGroupName = azd env get-value RESOURCE_GROUP
+$loadTestResourceName = azd env get-value LOADTESTING_NAME
 $jmxFilePath = './loadtest/httppost.jmx'
-$subnetId = $env:LOAD_SUBNET_ID
-$functionAppName = $env:AZURE_FUNCTION_NAME
+$subnetId = azd env get-value LOAD_SUBNET_ID
+$functionAppName = azd env get-value AZURE_FUNCTION_NAME
 
 # Create a load test with subnet details and environment variable for Function App name
 az load test create --resource-group $resourceGroupName --name $loadTestResourceName --test-id e2e-demo-test --test-plan $jmxFilePath --test-type JMX --autostop-error-rate 10.0 --subnet-id $subnetId --env FUNCTION_APP_NAME=$functionAppName
